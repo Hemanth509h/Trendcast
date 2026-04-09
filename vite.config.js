e9587d639c9bbef3f5f925e7382fa285d9fd1770
Hemanth kumar <phemanthkumar509@gmail.com>
2026-04-09 15:00:50 +0530

    f

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-<<<<<<< HEAD
     host: true,
-  
+
     watch: {
       usePolling: true,
       interval: 300,
     },
 
-=======
-    host: "0.0.0.0",
-    port: 5000,
-    allowedHosts: true,
->>>>>>> hemanth
     proxy: {
       "/api": {
         target: "http://localhost:8000",
